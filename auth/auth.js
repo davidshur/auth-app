42a0a6cd342c7bf8ccd0b45b948afb073ca5ab41
David <david@thejurne.com>
2020-07-23 13:57:44 -0700

    add jwt strat

--- auth/auth.js
+++ auth/auth.js
@@ -1,5 +1,7 @@
 const passport = require('passport');
 const localStrategy = require('passport-local');
+const JWTStrategy = require('passport-jwt').Strategy;
+const { ExtractJwt } = require('passport-jwt');
 const UserModel = require('../models/model');
 
 passport.use('signup', new localStrategy({
@@ -15,23 +17,33 @@ passport.use('signup', new localStrategy({
 }));
 
 passport.use('login', new localStrategy({
-  usernameField: 'email',
-  passwordField: 'password'
+  usernameField : 'email',
+  passwordField : 'password'
 }, async (email, password, done) => {
   try {
     const user = await UserModel.findOne({ email });
-    if (!user) {
-      return done(null, false, { message: 'User not found...'});
-    }
+    if( !user ){
+      return done(null, false, { message : 'User not found...'});
+    };
 
     const validate = await user.isValidPassword(password);
-    if (!validate) {
-      return done(null false, { message: 'Wrong password...'});
+    if( !validate ){
+      return done(null, false, { message : 'Wrong password...'});
     }
 
-    return done(null, user, { message: 'Logged in succesfully!'});
+    return done(null, user, { message : 'Logged in successfully.'});
   } catch (error) {
     return done(error);
   }
+}));
 
-}))
+passport.use(new JWTStrategy({
+  secretOrKey: 'little_mouse',
+  jwtFromRequest: ExtractJwt.fromUrlQueryParameter('secret_token')
+}, async (token, done) => {
+  try {
+    return done(null, user);
+  } catch (error) {
+    done(error);
+  }
+}));
