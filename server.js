f8203ea7b5b5906d9fa30022b062a9d18a01def0
David <david@thejurne.com>
2020-07-23 18:09:07 -0700

    add routes to server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
 
 const app = express();
 
@@ -15,6 +18,12 @@ mongoose
   })
   .then(() => console.log(`DB connected..`))
   .catch((err) => console.log(err));
+
+app.use(passport.initialize());
+
+require('./config/passport')(passport);
+
+app.use('/api/users', users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port: ${port}!`));
