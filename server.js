8bd0fd9dff0f154e94f4ff578e1a43f049391f69
David <david@thejurne.com>
2020-07-24 14:29:24 -0700

    add server based

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use('/api/users', users);
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/build/index.html'));
+  res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
 });
 
 app.listen(port, () => console.log(`Server running on port: ${port}!`));
