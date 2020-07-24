41c705ae9155b93495e637607538cbdb1b53c266
David <david@thejurne.com>
2020-07-24 14:59:24 -0700

    add fix

--- server.js
+++ server.js
@@ -30,13 +30,13 @@ mongoose
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
+app.use('/api/users', users);
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+  });
 }
-
-app.use('/api/users', users);
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-});
 
 app.listen(port, () => console.log(`Server running on port: ${port}!`));
