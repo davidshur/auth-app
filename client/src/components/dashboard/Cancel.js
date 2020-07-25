c57ccb284048601bb41c00b3d4feb02815a43bc3
David <david@thejurne.com>
2020-07-24 17:19:34 -0700

    add succ, canc

--- client/src/components/dashboard/Cancel.js
+++ client/src/components/dashboard/Cancel.js
@@ -2,46 +2,20 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { logoutUser } from '../../actions/authActions';
-import { loadStripe } from '@stripe/stripe-js';
 
-const stripePromise = loadStripe(
-  'pk_live_51H8XMFDrfM58ozRH5XQQwYxzfmjhvAVPlwtMyDwVfdqo5bJcEYJgzVnQzBdAUybX9OIh0Bph1WTFSW9FBUS41Ta400Pk8FEBYL'
-);
-
-class Dashboard extends Component {
+class Cancel extends Component {
   onLogoutClick = (e) => {
     e.preventDefault();
     this.props.logoutUser();
   };
 
-  handleClick = async (e) => {
-    // When the customer clicks on the button, redirect them to Checkout.
-    const stripe = await stripePromise;
-    stripe.redirectToCheckout({
-      lineItems: [
-        {
-          price: 'price_1H8aKRDrfM58ozRHxSJMYVjx',
-          quantity: 1,
-        },
-      ],
-      mode: 'payment',
-      successUrl: 'http://auth-app-mern.herokuapp.com/success',
-      cancelUrl: 'http://auth-app-mern.herokuapp.com/cancel',
-    });
-  };
-
   render() {
-    const { user } = this.props.auth;
     return (
       <div style={{ height: '75vh' }} className="container valign-wrapper">
         <div className="row">
           <div className="col s12 center-align">
             <h4>
-              <b>Hey there,</b> {user.name.split(' ')[0]}
-              <p className="flow-text grey-text text-darken-1">
-                It is time you showed <span style={{ fontFamily: 'monospace' }}>DAVID</span> your
-                support 👏
-              </p>
+              <p className="flow-text grey-text text-darken-1">Maybe next time?</p>
             </h4>
             <div className="col s6">
               <button
@@ -78,7 +52,7 @@ class Dashboard extends Component {
   }
 }
 
-Dashboard.propTypes = {
+Cancel.propTypes = {
   logoutUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
 };
@@ -87,4 +61,4 @@ const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, { logoutUser })(Dashboard);
+export default connect(mapStateToProps, { logoutUser })(Cancel);
